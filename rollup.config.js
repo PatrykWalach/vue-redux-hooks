988a7a7d19adf40559a3b46fb2889a7cdfc8d492
Patryk Wałach <patryk_walach@outlook.com>
2019-12-07 18:11:08 +0100

    feat: remove commonjs plugin

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,15 @@
-import commonjs from 'rollup-plugin-commonjs'
 import pkg from './package.json'
 import resolve from 'rollup-plugin-node-resolve'
 import { terser } from 'rollup-plugin-terser'
 import typescript from 'rollup-plugin-typescript2'
+const outputOptions = {
+  exports: 'named',
+  globals: {
+    '@vue/composition-api': 'vueCompositionApi',
+    redux: 'Redux',
+    vue: 'Vue',
+  },
+}
 export default {
   external: [
     ...Object.keys(pkg.dependencies || {}),
@@ -11,20 +18,27 @@ export default {
   input: './src/index.ts',
   output: [
     {
+      ...outputOptions,
       file: pkg.main,
       format: 'cjs',
     },
     {
+      ...outputOptions,
       file: pkg.module,
       format: 'es',
     },
+    {
+      ...outputOptions,
+      file: pkg.unpkg,
+      format: 'umd',
+      name: 'vueReduxHooks',
+    },
   ],
   plugins: [
-    resolve(),
-    commonjs(),
     typescript({
       typescript: require('typescript'),
     }),
+    resolve(),
     terser(),
   ],
 }
