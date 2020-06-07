abce374781742ff45e9a372eefbde17f7aa91d97
PatrykWalach <patryk_walach@outlook.com>
2020-06-07 12:30:35 +0200

    feat: add error if store not provided

--- src/hooks/useStore.ts
+++ src/hooks/useStore.ts
@@ -2,4 +2,17 @@ import { ReduxStore } from '../key'
 import { Store } from 'redux'
 import { inject } from 'vue'
 
-export const useStore = <S extends Store = Store>() => inject(ReduxStore) as S
+export const useStore = <S extends Store = Store>() => {
+  const store = inject<S>(ReduxStore)
+  assert(
+    store,
+    'Warning: no redux store was provided.\n\nPlease provide store preferably with vue install\n\napp.provide(ReduxStore, store)\n\nLearn more about vue-redux-hooks: https://github.com/PatrykWalach/vue-redux-hooks',
+  )
+  return store
+}
+
+function assert(condition: any, message: string): asserts condition {
+  if (!condition) {
+    console.error(message)
+  }
+}
