5946ff73ec2e1435086fca071c07046819580fe0
PatrykWalach <patryk_walach@outlook.com>
2020-06-07 12:30:15 +0200

    feat: improve typing

--- src/hooks/useDispatch.ts
+++ src/hooks/useDispatch.ts
@@ -1,7 +1,9 @@
-import { Store } from 'redux'
+import { Dispatch, AnyAction } from 'redux'
 import { useStore } from './useStore'
 
-export const useDispatch = <S extends Store = Store>() => {
-  const { dispatch } = useStore<S>()
-  return dispatch
+export const useDispatch = <
+  D extends Dispatch<AnyAction> = Dispatch<AnyAction>
+>() => {
+  const { dispatch } = useStore()
+  return dispatch as D
 }
