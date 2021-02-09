aeb80417007384546abb00875b73604771d223c7
PatrykWalach <patryk_walach@outlook.com>
2021-02-09 21:52:45 +0100

    feat: update dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,11 @@ module.exports = {
   env: {
     node: true,
   },
-  extends: ['plugin:@typescript-eslint/recommended','prettier/@typescript-eslint','plugin:prettier/recommended'],
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
+  ],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
