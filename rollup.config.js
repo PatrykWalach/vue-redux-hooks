d7409808a38a5691ac757ef20e897c94861a4a06
PatrykWalach <patryk_walach_github@outlook.com>
2021-07-13 16:43:50 +0200

    feat: use rollup

---
+++ rollup.config.js
@@ -0,0 +1,40 @@
+import typescript from '@rollup/plugin-typescript'
+import dts from 'rollup-plugin-dts'
+import pkg from './package.json'
+
+export default [
+  {
+    input: 'src/index.ts',
+
+    output: [
+      {
+        file: pkg.module,
+        format: 'esm',
+        sourcemap: true,
+      },
+      {
+        file: pkg.main,
+        format: 'cjs',
+        sourcemap: true,
+      },
+      // {
+      //   file: pkg.unpkg,
+      //   format: 'umd',
+      //   name: 'VueReduxHooks',
+      //   sourcemap: true,
+      //   globals: {
+      //     'vue': 'Vue',
+      //   },
+      // },
+    ],
+
+    plugins: [typescript()],
+
+    external: ['vue', '@reduxjs/toolkit', '@reduxjs/toolkit/query'],
+  },
+  {
+    input: './dist/dts/index.d.ts',
+    output: [{ file: 'dist/index.d.ts', format: 'es' }],
+    plugins: [dts()],
+  },
+]
