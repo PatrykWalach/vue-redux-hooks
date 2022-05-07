d9ec69ca0919fe48c2c6b7651540d5a2d3ee1996
Patryk Wałach <35966385+PatrykWalach@users.noreply.github.com>
2022-05-07 23:25:09 +0200

    refactor: get action

--- src/hooks/useDispatch.ts
+++ src/hooks/useDispatch.ts
@@ -2,14 +2,14 @@ import { Action, AnyAction, Dispatch } from 'redux'
 import { ComponentCustomProperties } from 'vue-demi'
 import { useStore } from './useStore'
 
-export type GetDispatch = ComponentCustomProperties extends {
-  $redux: { store: { dispatch: infer U } }
+export type GetAction = ComponentCustomProperties extends {
+  $redux: { store: { dispatch: Dispatch<infer U> } }
 }
   ? U
-  : Dispatch<AnyAction>
+  : AnyAction
 
 export function useDispatch<A extends Action = AnyAction>(): Dispatch<A>
-export function useDispatch<TDispatch = GetDispatch>(): TDispatch
+export function useDispatch<TDispatch = Dispatch<AnyAction>>(): TDispatch
 export function useDispatch() {
   return useStore().dispatch
 }
