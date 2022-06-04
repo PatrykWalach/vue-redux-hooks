f32f133be6a84ecdc93c54242178bf0fcc363b40
Patryk Wałach <35966385+PatrykWalach@users.noreply.github.com>
2022-06-04 12:32:10 +0200

    chore(test): add coverage

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,8 +1,14 @@
 import { defineConfig } from 'cypress'
 import viteConfig from './vite.config'
+import codeCoverage from '@cypress/code-coverage/task'
 
 export default defineConfig({
   component: {
+    setupNodeEvents(on, config) {
+      codeCoverage(on, config)
+
+      return config
+    },
     devServer: {
       framework: 'vue',
       bundler: 'vite',
